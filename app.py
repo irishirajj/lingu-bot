627b7e6f8c3cc7321a5cba35a46af50096c523b9
Rishi <rishiraj.mec18@itbhu.ac.in>
2022-07-07 15:25:15 +0530

    "Initial Commit"

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler,Filters
 import os
 TOKEN=os.environ.get("TELEGRAM_ID")
-
+#TOKEN="5594910284:AAG_GPQddNbfJGjgr0av1xcDadwXQc5X9Sw"
 def start(update,context):
     yourname=update.message.chat.first_name
     msg="Hi"+yourname+"! Welcome to Lingua Franca Vocab Bot"
@@ -22,15 +22,17 @@ def main():
 
     ###################   Handlers :
     dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("details", details))
+
     dp.add_handler(MessageHandler(Filters.text,mimic))
-    dp.add_handler(CommandHandler("details",details))
+
     dp.add_error_handler(error)
 
-    #updater.start_polling()
+    updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",
                           port=os.environ.get("PORT",443),
                           url_path=TOKEN,
-                          webhook_url="https://lingu-bot.herokuapp.com"+TOKEN
+                          webhook_url="https://lingu-bot.herokuapp.com/"+TOKEN
                           )
 
     updater.idle()

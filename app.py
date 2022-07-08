e3c67ca5885cc90078546b5dc5cd4a29d6c944c0
Rishi <rishiraj.mec18@itbhu.ac.in>
2022-07-09 03:56:25 +0530

    "Initial Commit"

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from telegram import Update
 from telegram.ext import CallbackContext, ApplicationHandlerStop, TypeHandler, Application
 SPECIAL_USERS = [2060060048, 172380183, 1827979793]  # Allows users
 
-TOKEN="5332532839:AAHkaINudmaGOwCMWXW9-IBFqY3ye-SMmeE"
+TOKEN="5594910284:AAG_GPQddNbfJGjgr0av1xcDadwXQc5X9Sw"
 def start(update,context):
     yourname=update.message.chat.first_name
     msg="Hi"+yourname+"! Welcome to Lingua Franca Vocab Bot"
@@ -22,6 +22,7 @@ async def callback(update: Update, context: CallbackContext):
     if update.effective_user.user_id in SPECIAL_USERS:
         pass
     else:
+        context.bot.send_message(update.message.chat.id, update.message.text)
         await update.effective_message.reply_text("Hey! You are not allowed to use me!")
         raise ApplicationHandlerStop
 
@@ -40,7 +41,7 @@ dp.add_handler(CommandHandler("details", details))
 
 
 dp.add_error_handler(error)
-app = Application.builder().token("TOKEN").build()
+app = Application.builder().token(TOKEN).build()
 handler = TypeHandler(Update, callback) # Making a handler for the type Update
 app.add_handler(handler, -1) # Default is 0, so we are giving it a number below 0
 # Add other handlers and start your bot.
